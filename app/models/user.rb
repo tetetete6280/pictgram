e117fce4bfe46bbb7fd0398dfa0a7d5c99282b99
tetetete6280 <finance6280@gmail.com>
2021-11-23 11:31:34 +0000

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   validates :password, presence: true, length: {minimum: 8, maximum: 32}, format:{with: VALID_PASSWORD_REGEX}
-
+#test
 
 #VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{8,32}\z/
  # validates :password, presence: true,
